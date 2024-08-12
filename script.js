1ecac9c792dad7f2b16d2b7ce823938455efdb0b
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-08-12 21:28:03 +0530

    start

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your interest! We will get back to you soon.');
+});
