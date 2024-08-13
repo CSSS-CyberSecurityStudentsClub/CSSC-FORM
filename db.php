7f22717d35e3e85e4585d0dcd39a0906a4b69303
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-08-13 08:54:15 +0530

    new server

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql110.infinityfree.com";  // Replace with your database server
-$username = "if0_37093939";         // Replace with your MySQL username
-$password = "RaxLord19";             // Replace with your MySQL password
-$dbname = "if0_37093939_club_database";  // Replace with your database name
+$servername = "sql110.ezyro.com";  // Replace with your database server
+$username = "ezyro_37095098";         // Replace with your MySQL username
+$password = "06f45a87d74bf4";             // Replace with your MySQL password
+$dbname = "ezyro_37095098_club_database";  // Replace with your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
