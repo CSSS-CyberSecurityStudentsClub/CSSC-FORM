05d2557155c835106e7526f880ef27ead83e17ce
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-08-13 20:10:26 +0530

    try

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// document.getElementById('signup-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     alert('Thank you for your interest! We will get back to you soon.');
-// });
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbw0sAQ0AP-Z7hfu1HyGBsoTXEcLRCaPFEf4IsaTDhSjEwBdlpXT9HO9PTJLLZwc4T_F/exec'
-const form = document.forms['contact-form']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw0sAQ0AP-Z7hfu1HyGBsoTXEcLRCaPFEf4IsaTDhSjEwBdlpXT9HO9PTJLLZwc4T_F/exec';
+const form = document.forms['contact-form'];
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(response => {
-      alert("Thank you! Your form is submitted successfully.");
-      form.reset(); // Optionally clear the form fields
+      if (response.ok) {
+        alert("Thank you! Your form is submitted successfully.");
+        window.location.href = 'https://rakeshyadav-19.github.io/CLUB-FORM/'; // Redirect to the URL
+      } else {
+        throw new Error('Network response was not ok.');
+      }
     })
     .catch(error => {
       console.error('Error!', error.message);
