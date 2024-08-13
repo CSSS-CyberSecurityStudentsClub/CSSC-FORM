3dd9604c5efc337b301e37144e4da8964086b7d2
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-08-13 23:02:00 +0530

    async wala

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw0sAQ0AP-Z7hfu1HyGBsoTXEcLRCaPFEf4IsaTDhSjEwBdlpXT9HO9PTJLLZwc4T_F/exec';
 const form = document.forms['contact-form'];
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', async e => {
   e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then(response => {
-      if (response.ok) {
-        alert("Thank you! Your form is submitted successfully.");
-        window.location.href = 'https://rakeshyadav-19.github.io/CLUB-FORM/'; // Redirect to the URL
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .catch(error => {
-      console.error('Error!', error.message);
-      alert("Sorry, there was an error. Please try again.");
-    });
+  try {
+    const response = await fetch(scriptURL, { method: 'POST', body: new FormData(form) });
+    
+    if (response.ok) {
+      alert("Thank you! Your form is submitted successfully.");
+      window.location.href = 'https://rakeshyadav-19.github.io/CLUB-FORM/'; // Redirect to the URL
+    } else {
+      throw new Error('Network response was not ok.');
+    }
+  } catch (error) {
+    console.error('Error!', error.message);
+    alert("Sorry, there was an error. Please try again.");
+  }
 });
